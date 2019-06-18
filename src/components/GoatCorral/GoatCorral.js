2f5eefe34993f1ef70f693f61d3e66dd97349354
Lwam Tekie <ltekie87@yahoo.com>
2019-06-17 23:39:14 -0500

    wip

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import goatShape from '../../helpers/propz/goatShape';
+
+import Goat from '../Goat/Goat';
+
 import './GoatCorral.scss';
 
-
 class GoatCorral extends React.Component {
+  static propTypes = {
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+  }
+
   render() {
     const { goats } = this.props;
     const makeGoats = goats.map(goat => (
-    <h2>{goat.name}</h2>
-    // <Goat goat={goat} />
+      <Goat key={goat.id} goat={goat} />
     ));
     return (
-      <div>
-      { makeGoats }
+      <div className="d-flex flex-wrap">
+        { makeGoats }
       </div>
     );
   }
