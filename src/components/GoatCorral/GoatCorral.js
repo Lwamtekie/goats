195e7232dcd85de03cff070a04c8b1f5079e7629
Lwam Tekie <ltekie87@yahoo.com>
2019-06-17 23:25:04 -0500

    single goats

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './GoatCorral.scss';
 
+import Goat from '../Goat/Goat';
+
+import './GoatCorral.scss';
 
 class GoatCorral extends React.Component {
   render() {
     const { goats } = this.props;
     const makeGoats = goats.map(goat => (
-    <h2>{goat.name}</h2>
-    // <Goat goat={goat} />
+      <Goat key={goat.id} goat={goat} />
     ));
+
     return (
-      <div>
-      { makeGoats }
+      <div className="GoatCorral d-flex flex-wrap">
+        { makeGoats }
       </div>
     );
   }
